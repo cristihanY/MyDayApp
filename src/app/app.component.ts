3c70a698a66c0dcb0318d850c4bd338475763084
Cristihan <cristihan.yanqui.27@unsch.edu.pe>
2023-11-03 12:04:11 -0500

    test completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: '<app-home></app-home>',
 })
 export class AppComponent {}
